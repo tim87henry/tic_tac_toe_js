e61aac45076682fba074be1ba8b96e93d139d539
Timothy <tim87henry@gmail.com>
2020-12-09 22:23:38 +1100

    Added AI gameplay

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 var gameBoard=document.querySelector("#gameboard");
 let gameSquare=[];
 let gameActive=false;
+let currentUser;
+let user1;
+let user2;
+let p1_ai=false;
+let p2_ai=false;
 
 const Board = (() => {
     const display = () => {
@@ -15,7 +20,7 @@ const Board = (() => {
     return {display};
 })();
 
-const User = (num,name) => {
+const User = (num,name,isAI) => {
     let mark = (num===1)? "X" : "O"; 
     
     const drawMark = (squareId) => {
@@ -52,40 +57,78 @@ const User = (num,name) => {
         return false;
     }
 
-    return {drawMark, name, isGameOver, isGameTie};
+    const makeMove = () => {
+        if (isAI && gameActive) {
+            console.log("AI taking over")
+            let values=[];
+            for (i=0;i<9;i++) {
+                values[i]=document.querySelector("#square"+i).innerHTML;
+            }
+            randomSquare=Math.floor(Math.random()*9)
+            while (values[randomSquare]!='') {
+                randomSquare=Math.floor(Math.random()*9)
+            }
+            console.log("AI brain says "+randomSquare+" for player "+num)
+            drawMark("square"+randomSquare);
+            if (currentUser.isGameOver()==true) {
+                result.innerHTML=currentUser.name+" won";
+                gameActive=false;
+            } else if (currentUser.isGameTie()==true) {
+                result.innerHTML="Tie Game";
+                gameActive=false;
+            }
+            currentUser = (currentUser===user1)? user2 : user1;
+            currentUser.makeMove();
+        } else {
+            gameBoard.addEventListener("click",function(e) {
+            console.log("onclick board")
+            if (gameActive) {
+                let squareId=e.target.id;
+                if (document.querySelector("#"+squareId).innerHTML!="X" && document.querySelector("#"+squareId).innerHTML!="O") {
+                    currentUser.drawMark(squareId);
+                    if (currentUser.isGameOver()==true) {
+                        result.innerHTML=currentUser.name+" won";
+                        gameActive=false;
+                    } else if (currentUser.isGameTie()==true) {
+                        result.innerHTML="Tie Game";
+                        gameActive=false;
+                    }
+                    currentUser = (currentUser===user1)? user2 : user1;
+                    currentUser.makeMove();
+                }
+            }
+            })
+        }
+    }
+
+    return {drawMark, name, isAI, isGameOver, isGameTie, makeMove};
 };
 
 const gamePlay = (() => {
-    let currentUser;
-    let user1;
-    let user2;
+    let ai_button_bg="lightseagreen";
+    let ai_button_bg_clicked="red";
     var result=document.querySelector("#result");
     var startGame=document.querySelector("#start");
+    var ai1=document.querySelector("#ai1");
+    var ai2=document.querySelector("#ai2");
+    ai1.addEventListener("click",function(e) {
+        p1_ai = (p1_ai===false)? true: false;
+        ai1.style.background = (p1_ai===false)? ai_button_bg:ai_button_bg_clicked;
+    });
+    ai2.addEventListener("click",function(e) {
+        p2_ai = (p2_ai===false)? true: false;
+        ai2.style.background = (p2_ai===false)? ai_button_bg:ai_button_bg_clicked;
+    });
     startGame.addEventListener("click",function(e) {
         Board.display();
-        let player1=document.getElementById("player1").value;
-        let player2=document.getElementById("player2").value;
-        user1=User(1,player1);
-        user2=User(2,player2);
+        let player1= (p1_ai)? "AI Player 1":document.getElementById("player1").value;
+        let player2= (p2_ai)? "AI Player 2":document.getElementById("player2").value;
+        user1=User(1,player1,p1_ai);
+        user2=User(2,player2,p2_ai);
         currentUser=user1;
         gameActive=true;
+        currentUser.makeMove();
     });
-    gameBoard.addEventListener("click",function(e) {
-        if (gameActive) {
-            let squareId=e.target.id;
-            if (document.querySelector("#"+squareId).innerHTML!="X" && document.querySelector("#"+squareId).innerHTML!="O") {
-                currentUser.drawMark(squareId);
-                if (currentUser.isGameOver()==true) {
-                    result.innerHTML=currentUser.name+" won";
-                    gameActive=false;
-                } else if (currentUser.isGameTie()==true) {
-                    result.innerHTML="Tie Game";
-                    gameActive=false;
-                }
-                currentUser = (currentUser===user1)? user2 : user1;
-            }
-        }
-    })
 })();
 
 
